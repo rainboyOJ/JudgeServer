e7ef113249d999bd7a3b4c786633cb23820b8886
rainboy <rainboylvx@qq.com>
2019-08-23 15:50:44 +0000

    [add] 主页的显示

--- src/app/Koa.ts
+++ src/app/Koa.ts
@@ -1,14 +1,21 @@
 import * as Koa from 'koa'
 import * as logger from 'koa-logger'
+import * as views from 'koa-views'
 
 /** 产生一个 Koa 实例 */
 export const createServer = ():Koa => {
     var app = new Koa()
     app.use(logger())
+    app.use(views("views",{
+        extension:'pug'
+    }))
 
     app.use(async function(ctx,next){
-        if( ctx.method == 'GET' && ctx.path === '/')
-            ctx.body = 'judgeServer Index'
+        if( ctx.method == 'GET' && ctx.path === '/'){
+            await ctx.render('index',{
+                content:'hello world, JudgeServer!'
+            })
+        }
         else
             await next();
     })
