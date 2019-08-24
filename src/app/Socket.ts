50adfa7b0940ae1ee8f36ff883bcd26e6de30624
rainboy <rainboylvx@qq.com>
2019-08-24 17:30:36 +0000

    [完善] socket连接需要token

--- src/app/Socket.ts
+++ src/app/Socket.ts
@@ -2,11 +2,25 @@ import * as socket from 'socket.io'
 import * as Koa from 'koa'
 import * as http from 'http'
 import debug from '../lib/debug'
+import config from '../lib/CONFIG'
 
 
 export const createSocket = (app:Koa) => {
     const server = http.createServer(app.callback())
     const io = socket(server)
+
+    io.use((socket, next) => {
+        let {query:{token}}= socket.handshake;
+        let {TOKEN} = <CONFIG>config.get_config()
+        if( token == TOKEN){
+            next()
+        }
+        else {
+            console.error(`close ${socket.id} connection, \n\t reason: invalid token`)
+            debug(` query token: ${token}\n config token: ${TOKEN}`)
+            socket.disconnect()
+        }
+    });
 
     
     io.on('connection', function(socket){
--- test/connect.ts
+++ test/connect.ts
@@ -1,6 +1,6 @@
 import * as Client from 'socket.io-client'
 
-const client = Client("http://127.0.0.1:5000")
+const client = Client("http://127.0.0.1:5000?token=token")
 
 client.on('connect',()=>{
     console.log('connected!')
