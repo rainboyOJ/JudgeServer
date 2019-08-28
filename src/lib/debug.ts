bef5eee0110f39b0c2e8f6f03e32ca26899014f0
rainboy <rainboylvx@qq.com>
2019-08-28 23:31:57 +0800

    chore: 升级 debug

--- src/lib/debug.ts
+++ src/lib/debug.ts
@@ -1,34 +1,65 @@
 import debug from 'debug'
+import {basename} from 'path'
+
 
 class mydebug {
-    debug: debug.Debugger
-    detail: debug.Debugger
-    __line : any
+    DEBUG: debug.Debugger
+    DETAIL: debug.Debugger
+    private STACK_FRAME_RE:RegExp
+    private THIS_FILE:string
     constructor(){
-        this.debug = debug('debug')
-        this.detail = debug('detail')
+        this.DEBUG = debug('debug')
+        this.DETAIL = debug('detail')
+        this.STACK_FRAME_RE = new RegExp(/at ((\S+)\s)?\(?([^:]+):(\d+):(\d+)/);
+        this.THIS_FILE = __filename.split('/')[__filename.split('/').length - 1];
 
-        var self = this
         // 来自 https://stackoverflow.com/questions/11386492/accessing-line-number-in-v8-javascript-chrome-node-js
-        Object.defineProperty(this, '__stack', {
-            get: function(){
-                var orig = Error.prepareStackTrace;
-                Error.prepareStackTrace = function(_, stack){ return stack; };
-                var err = new Error;
-                Error.captureStackTrace(err, arguments.callee);
-                var stack = err.stack;
-                Error.prepareStackTrace = orig;
-                return stack;
-            }
-        });
         
-        Object.defineProperty(self, '__line', {
-            get: function(){
-                //@ts-ignore
-                return self.__stack[1].getLineNumber();
-            }
-        });
+        //Object.defineProperty(self, '__line', {
+        //});
+    }
 
+    info(msg:any){
+        this.DEBUG(msg)
+    }
+
+    debug(msg:any){
+        let callerInfo = this._getCaller() || {}
+        //@ts-ignore
+        this.DEBUG(`===== module :[${callerInfo.module}] \n`, `===== function :[${callerInfo.function}] \n`,`===== line: [${callerInfo.line}] \n`, msg)
+    }
+    detail(msg:any){
+        let callerInfo = this._getCaller() || {}
+        //@ts-ignore
+        this.DETAIL(callerInfo.module, callerInfo.function, callerInfo.line, msg,)
+    }
+
+    //https://gist.github.com/jedp/3166317
+    _getCaller() {
+        var err = new Error();
+        Error.captureStackTrace(err);
+
+        // Throw away the first line of the trace
+        // @ts-ignore
+        var frames = err.stack.split('\n').slice(1);
+
+        // Find the first line in the stack that doesn't name this module.
+        var callerInfo = null;
+        for (var i = 0; i < frames.length; i++) {
+            if (frames[i].indexOf(this.THIS_FILE) === -1) {
+                callerInfo = this.STACK_FRAME_RE.exec(frames[i]);
+                break;
+            }
+        }
+        if (callerInfo) {
+            return {
+                function: callerInfo[2] || null,
+                module: callerInfo[3] ? basename(callerInfo[3]): null,
+                line: callerInfo[4] || null,
+                column: callerInfo[5] || null
+            };
+        }
+        return null;
     }
 }
 
