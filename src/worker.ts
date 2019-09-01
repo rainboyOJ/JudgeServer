28a906810f0f7ce82dbd8e88f8398308d6584e82
rainboy <rainboylvx@qq.com>
2019-09-01 11:38:49 +0800

    style: 优化代码格式

--- src/worker.ts
+++ src/worker.ts
@@ -27,54 +27,54 @@ var compile_routes = routeIns.create('/compile',[
     "compile.generate_each_point_judge_args"//生成每个测试点的 ctx
 ])
 
+//@ts-ignore
+var judge_routes = routeIns.create('/judge', [])
+
 
 async function main(){
     while(1){
 
-        let judge_ctx:CTX.ctx | null = await Redis.judge_pop()
-        if( judge_ctx != null){
+        let pop_ctx:CTX.ctx | null 
+        let socket_client_id:string
+
+        pop_ctx = await Redis.judge_pop()
+        if(!pop_ctx){
             debug.info('取出 judge_queue 里的数据')
-            debug.detail(judge_ctx)
-
-            await Redis.PUBLISH_MESSAGE({
-                socket_client_id:judge_ctx.config!.socket_client_id,
-                message:'',
-                result:0
-            })
-
+            pop_ctx = await Redis.compile_pop()
         }
-        else{
-            debug.debug('没有 从取出 judge_queue 里的数据')
-            let compile_ctx:CTX.ctx |null = await Redis.compile_pop()
-            if( compile_ctx != null){
-                debug.debug('取出 compile_queue 里的数据')
-                debug.detail(compile_ctx)
 
-                try {
-                    //@ts-ignore
-                    await compile_routes.routes()(compile_ctx, async (ctx:any)=>{ 
+        if( !pop_ctx){
+            debug.info(`什么数据也没有取出!`)
+        }
+        else {
+            socket_client_id = pop_ctx.config.socket_client_id
+            try {
+                //@ts-ignore
+                await compile_routes.routes()(pop_ctx,async (ctx:CTX.ctx | undefined )=>{
+                    if(ctx){
                         console.log("============ compile_routes exec END ==================================")
                         console.log(ctx)
-                        console.log("==================================================================")
-                    })
-                    await Redis.judge_push(compile_ctx)
-                }
-                catch(e){
-                    Redis.PUBLISH_MESSAGE({
-                        socket_client_id:compile_ctx!.config.socket_client_id,
-                        result: e.result || -1,
-                        message:e.message || e,
-                        result_list:[]
-                    });
-                }
-
-
+                        console.log("==================================================================") }
+                })
+                //@ts-ignore
+                await judge_routes.routes()(pop_ctx,async (ctx:CTX.ctx | undefined)=>{
+                    /** 评测结束,进行相关处理 */
+                    if( ctx) {
+                        console.log(ctx)
+                        // if ctx.judge_end === true
+                        // REDIS.PUBLISH_MESSAGE
+                    }
+                })
             }
-            else {
-                debug.debug('没有 从取出 compile_queue 里的数据')
+            catch(e){
+                Redis.PUBLISH_MESSAGE({
+                    socket_client_id:socket_client_id,
+                    result: e.result || -1,
+                    message:e.message || e,
+                    result_list:[]
+                });
             }
         }
-        
         await delay(1000);
     }
 }
